cd699d728a4836e689c745ecd4ea29a0b7b194f6
RUIXIANG XIONG <2540725x@student.gla.ac.uk>
2021-07-27 23:42:41 +0100

    completed chapter4 exercises

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    context_dic = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dic)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dic = {'name': 'Ruixiang Xiong'}
+    return render(request, 'rango/about.html', context=context_dic)
